b9de5f47388f076def8cb45e34b4048670837b72
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-11-06 18:55:12 -0400

    Change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Source Code Pro"'],
+        serif: ['"Source Code Pro"'],
+        mono: ['"Source Code Pro"'],
+        display: ['"Source Code Pro"'],
+        body: ['"Source Code Pro"'],
+      },
       listStyleType: {
         dash: "'-'",
       },
